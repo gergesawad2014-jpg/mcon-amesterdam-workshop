c9148225fc0b6b9ac59fceef1888fc1d5a87e9bb
gergesawad2014-jpg <gergesawad2014@gmail.com>
2026-04-21 11:29:18 +0000

    123

--- terraform-repo/variables.tf
+++ terraform-repo/variables.tf
@@ -2,21 +2,17 @@ variable "repository_name" {
   description = "The name of the GitHub repository to create"
   type        = string
 }
- 
- 
+
 variable "repository_description" {
   description = "A description for the GitHub repository"
   type        = string
 
-
-validation {
-    condition       = length(var.repository_description) >0
-    error_message   "the repository descriotion cannot be empty"
+  validation {
+    condition     = length(var.repository_description) > 0
+    error_message = "The repository description cannot be empty"
+  }
 }
-}
 
- 
- 
 variable "repository_private" {
   description = "Whether the GitHub repository should be private"
   type        = bool
